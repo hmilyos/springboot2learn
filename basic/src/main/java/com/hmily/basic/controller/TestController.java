c169ed783de7328495a1ae708cd5d29a2011a83c
oushiming <oushiming@sxt.com>
2019-05-05 23:46:39 +0800

    controller init

---
+++ basic/src/main/java/com/hmily/basic/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.hmily.basic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/msg")
+    public String msg(){
+        return "hello basic msg!";
+    }
+}
