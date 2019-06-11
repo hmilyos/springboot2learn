0c5b95353c99b9adc778294f79bd5758ed26a08f
oushiming <oushiming@xmiles.cn>
2019-06-11 09:18:41 +0800

    卡包系统初始化

---
+++ card-bag/src/main/java/com/hmily/cardbag/CardBagApplication.java
@@ -0,0 +1,13 @@
+package com.hmily.cardbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardBagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardBagApplication.class, args);
+    }
+
+}
